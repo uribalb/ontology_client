b3fb294845496a0a8051ba69358fd7b78911b5cb
Landry Bete <landrybete@gmail.com>
2019-02-07 11:26:14 +0100

    add ontology schema

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>Description de l'ontologie(mini-rapport)</h1>
+    <h1>Schéma général de l'ontologie</h1>
+    <v-flex xs12>
+      <v-slider v-model="width" :min="Math.min(windowWidth,800)" :max="windowWidth" step="1"></v-slider>
+
+      <v-img :width="width" src="@/assets/ontol.svg" class="my-1" contain></v-img>
+    </v-flex>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      width: 1000,
+      windowWidth: 1200
+    };
+  },
+
+  mounted() {
+    this.windowWidth = window.innerWidth;
+    this.width = 0.97 * this.windowWidth;
+    this.$nextTick(() => {
+      window.addEventListener("resize", () => {
+        this.windowWidth = window.innerWidth;
+      });
+    });
+  }
+};
+</script>
